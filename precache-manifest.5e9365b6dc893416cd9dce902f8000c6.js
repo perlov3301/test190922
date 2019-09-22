69b4a517cbd00fd58a1954bea572b79d115721b4
Shlomo-Alexander Perlov <perlov3301@gmail.com>
2019-09-22 06:52:51 +0300

    Updates

---
+++ precache-manifest.5e9365b6dc893416cd9dce902f8000c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9b71cc8c60043f4b6fa2d9866594cb8",
+    "url": "/test190922/index.html"
+  },
+  {
+    "revision": "cc0d04272dc8d0573137",
+    "url": "/test190922/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a3e947e9e0820b354b7e",
+    "url": "/test190922/static/js/2.17508ded.chunk.js"
+  },
+  {
+    "revision": "cc0d04272dc8d0573137",
+    "url": "/test190922/static/js/main.7142bb6b.chunk.js"
+  },
+  {
+    "revision": "dd6934cfb72c6a237f47",
+    "url": "/test190922/static/js/runtime-main.1b72adb3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/test190922/static/media/logo.25bf045c.svg"
+  }
+]);
